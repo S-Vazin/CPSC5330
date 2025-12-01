db4a693f7ac7ef00c71fdf0b2e36f7bbede733f2
S-Vazin <szv0073@auburn.edu>
2025-11-30 21:24:43 -0600

    Add files via upload
    Currency Converter app that allows users to enter in an integer to convert the value from usd to any combination of jpy, eur, gbp, and cad. The results are displayed on a second page.

---
+++ CurrencyConverterSV/AppTarget/AppTargetApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTargetApp.swift
+//  AppTarget
+//
+//  Created by Shawn Vazin on 11/30/25.
+//
+
+import SwiftUI
+
+@main
+struct AppTargetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
